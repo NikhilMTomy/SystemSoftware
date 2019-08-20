2918e81a5510a5db8c63b64eaa141667f7b8387c
NikhilMTomy <nikhilmtomy@gmail.com>
2019-08-20 06:36:37 +0530

    optimized

--- scheduling_algorithms.c
+++ scheduling_algorithms.c
@@ -1,25 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 struct Process {
 	int pid;
 	int burstTime;
-	int arrivalTime;
 	int waitingTime;
 	int turnaroundTime;
 	int priority;
 };
 /***********************************************************************************************/
-int fcfs_with_arrival_time(struct Process processList[], int processCount) {
-	int i, currentTime;
-	currentTime = 0;
-	for (i=0; i<processCount; i++) {
-		processList[i].waitingTime = currentTime - processList[i].arrivalTime;
-		currentTime += processList[i].burstTime;
-		processList[i].turnaroundTime = currentTime - processList[i].arrivalTime;
-	}
-	return 0;
-}
 int fcfs(struct Process processList[], int processCount) {
 	int i, currentTime;
 	currentTime = 0;
@@ -31,12 +19,12 @@ int fcfs(struct Process processList[], int processCount) {
 	return 0;
 }
 int sjf(struct Process processList[], int processCount) {
-	int i, j, currentTime, processLeft, totalBurstTime, shortestIdx;
+	int i, j, currentTime, shortestIdx;
 	currentTime = 0;
 	for (i=0; i<processCount; i++) {
 		shortestIdx = 0;
 		for (j=1; j<processCount; j++) {
-			if (processList[j].burstTime>0 && processList[j].burstTime<processList[shortestIdx].burstTime) {
+			if ((processList[j].burstTime>0 && processList[j].burstTime<processList[shortestIdx].burstTime) || processList[shortestIdx].burstTime==0) {
 				shortestIdx = j;
 			}
 		}
@@ -74,7 +62,7 @@ int roundRobin(struct Process processList[], int processCount) {
 				processList[i].waitingTime += quanta;
 			}
 		}
-		if ( processList[executionIdx].burstTime < quanta ) {
+		if ( processList[executionIdx].burstTime <= quanta ) {
 			currentTime += processList[executionIdx].burstTime;
 			processList[executionIdx].burstTime = 0;
 		} else {
@@ -94,16 +82,11 @@ int roundRobin(struct Process processList[], int processCount) {
 }
 int priority(struct Process processList[], int processCount) {
 	int i, currentTime, j, nextIndex, maxPriorityLeft;
-	printf("P\tBT\tPriority\n");
-	for (i=0; i<processCount; i++) {
-		printf("%d\t%d\t%d\n", i+1, processList[i].burstTime, processList[i].priority);
-	}
 	for (i=0, currentTime=0; i<processCount; i++)  {
 		nextIndex = -1, maxPriorityLeft=99;
 		for (j=0; j<processCount; j++) {
 			if (processList[j].burstTime > 0 && maxPriorityLeft > processList[j].priority) {
 				nextIndex = j;
-				printf("nextIndex : %d\n", nextIndex);
 				maxPriorityLeft = processList[j].priority;
 			}
 		}
@@ -116,64 +99,54 @@ int priority(struct Process processList[], int processCount) {
 	}
 	return 0;
 }
+int displayResults(struct Process processList[], int processCount) {
+	int i;
+	printf("P\tTAT\tWT\n");
+	for (i=0; i<processCount; i++) {
+		printf("%d\t%d\t%d\n", i+1, processList[i].turnaroundTime, processList[i].waitingTime);
+	}
+}
 /***********************************************************************************************/
 int main() {
-	int processCount, i, turnaroundTime, waitingTime;
-	int choice;
+	int processCount, i, turnaroundTime, waitingTime, choice;
 	struct Process processList[20];
 	printf("Enter number of processes : ");
 	scanf("%d", &processCount);
-	for (i=0; i<processCount; i++) {
-		printf("Process %d\n", i+1);
-		printf("Enter burst time : ");
-		scanf("%d", &processList[i].burstTime);
-		printf("Enter process priority : ");
-		scanf("%d", &processList[i].priority);
-		//printf("Enter arrival time : ");
-		//scanf("%d", &processList[i].arrivalTime);
-
-		processList[i].arrivalTime = 0;
-		processList[i].waitingTime = 0;
-		processList[i].turnaroundTime = 0;
-	}
 	printf("\nScheduling Algorithm\n");
 	printf("\t1. FCFS\n");
 	printf("\t2. SJF\n");
 	printf("\t3. Round Robin\n");
 	printf("\t4. Priority\n");
 	printf("Enter choice : ");
-	//fflush(stdin);
 	while ((choice = getchar()) == '\n');
-	//scanf("%c", &choice);
-	// choice = getchar();
+	for (i=0; i<processCount; i++) {
+		processList[i].pid = i;
+		printf("Process %d\n", i+1);
+		printf("Enter burst time : ");
+		scanf("%d", &processList[i].burstTime);
+		if (choice == '4') {
+			printf("Enter process priority : ");
+			scanf("%d", &processList[i].priority);
+		}
+		processList[i].waitingTime = 0;
+		processList[i].turnaroundTime = 0;
+	}
 	switch(choice) {
 		case '1':
 			fcfs(processList, processCount);
-			printf("P\tTAT\tWT\n");
-			for (i=0; i<processCount; i++) {
-				printf("%d\t%d\t%d\n", i+1, processList[i].turnaroundTime, processList[i].waitingTime);
-			}
+			displayResults(processList, processCount);
 			break;
 		case '2':
 			sjf(processList, processCount);
-			printf("P\tTAT\tWT\n");
-			for (i=0; i<processCount; i++) {
-				printf("%d\t%d\t%d\n", i+1, processList[i].turnaroundTime, processList[i].waitingTime);
-			}
+			displayResults(processList, processCount);
 			break;
 		case '3':
 			roundRobin(processList, processCount);
-			printf("P\tTAT\tWT\n");
-			for (i=0; i<processCount; i++) {
-				printf("%d\t%d\t%d\n", i+1, processList[i].turnaroundTime, processList[i].waitingTime);
-			}
+			displayResults(processList, processCount);
 			break;
 		case '4':
 			priority(processList, processCount);
-			printf("P\tTAT\tWT\n");
-			for (i=0; i<processCount; i++) {
-				printf("%d\t%d\t%d\n", i+1, processList[i].turnaroundTime, processList[i].waitingTime);
-			}
+			displayResults(processList, processCount);
 			break;
 		default:
 			printf("Invalid choice\n");
